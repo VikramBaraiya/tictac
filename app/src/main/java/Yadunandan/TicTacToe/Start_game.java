a3bcf0cc22c114a979c623537dc1790ece872971
VikramBaraiya <contactbaraiya@gmail.com>
2019-02-24 04:43:23 -0500

    crash resolved implemented layout for buttons to be cliked and also the signs to be printed

--- app/src/main/java/Yadunandan/TicTacToe/Start_game.java
+++ app/src/main/java/Yadunandan/TicTacToe/Start_game.java
@@ -2,14 +2,100 @@ package Yadunandan.TicTacToe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Start_game extends AppCompatActivity {
+public class Start_game extends AppCompatActivity implements View.OnClickListener{
 
+    static int count=0;
+    Button r11,r12,r13,r21,r22,r23,r31,r32,r33;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+        r11=findViewById(R.id.r11);
+        r12=findViewById(R.id.r12);
+        r13=findViewById(R.id.r13);
+        r21=findViewById(R.id.r21);
+        r22=findViewById(R.id.r22);
+        r23=findViewById(R.id.r23);
+        r31=findViewById(R.id.r31);
+        r32=findViewById(R.id.r32);
+        r33=findViewById(R.id.r33);
+/*r11.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+    }
+});*/
+        r11.setOnClickListener(this);
+        r12.setOnClickListener(this);
+        r13.setOnClickListener(this);
+        r21.setOnClickListener(this);
+        r22.setOnClickListener(this);
+        r23.setOnClickListener(this);
+        r31.setOnClickListener(this);
+        r32.setOnClickListener(this);
+        r33.setOnClickListener(this);
+
+
+
       }
+
+      public void toggle(Button b)
+      {
+          if(count!=9)
+          {
+              String value="";
+              b.setEnabled(false);
+              if(count%2==0)
+                  value="X";
+              else
+                  value="O";
+              b.setText(value);
+              count++;
+
+          }
+          else
+          {
+
+          }
+
+      }
+    @Override
+    public void onClick(View v) {
+        String value="";
+        switch (v.getId()){
+            case R.id.r11:
+                toggle(r11);
+               break;
+            case R.id.r12:
+                toggle(r12);
+                break;
+            case R.id.r13:
+                toggle(r13);
+                break;
+            case R.id.r21:
+                toggle(r21);
+                break;
+            case R.id.r22:
+                toggle(r22);
+                break;
+            case R.id.r23:
+                toggle(r23);
+                break;
+            case R.id.r31:
+                toggle(r31);
+                break;
+            case R.id.r32:
+                toggle(r32);
+                break;
+            case R.id.r33:
+                toggle(r33);
+                break;
+
+        }
+    }
 }
