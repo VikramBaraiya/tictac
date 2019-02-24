bc1c5fd28d74e691ae5afbd88010ff4400b3871b
VikramBaraiya <contactbaraiya@gmail.com>
2019-02-24 05:20:17 -0500

    project running successfully implemented checking win on all disabled all after win

--- app/src/main/java/Yadunandan/TicTacToe/Start_game.java
+++ app/src/main/java/Yadunandan/TicTacToe/Start_game.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class Start_game extends AppCompatActivity implements View.OnClickListener{
 
-    static int count=0;
+    int count=0;
     Button r11,r12,r13,r21,r22,r23,r31,r32,r33;
     String r11str,r12str,r13str,r21str,r22str,r23str,r31str,r32str,r33str;
 
@@ -62,7 +62,7 @@ public class Start_game extends AppCompatActivity implements View.OnClickListene
           }
           else
           {
-
+                count=0;
           }
 
       }
@@ -82,6 +82,24 @@ public class Start_game extends AppCompatActivity implements View.OnClickListene
 
 
       }
+
+      void disableall()
+      {
+          r11.setEnabled(false);
+          r12.setEnabled(false);
+          r13.setEnabled(false);
+          r21.setEnabled(false);
+          r22.setEnabled(false);
+          r23.setEnabled(false);
+          r31.setEnabled(false);
+          r32.setEnabled(false);
+          r33.setEnabled(false);
+
+
+
+
+
+      }
     @Override
     public void onClick(View v) {
         String value= "";
@@ -95,10 +113,16 @@ public class Start_game extends AppCompatActivity implements View.OnClickListene
                 {
   //                  r11str=r11.getText().toString();
                          updateText();
-                         if(r11str.equals(r12str) && r11str.equals(r13str))
+                         if(
+                                 (r11str.equals(r12str) && r11str.equals(r13str))
+                                         ||(r11str.equals(r21str) && r11str.equals(r31str))
+                                         || (r11str.equals(r22str) && r11str.equals(r33str))
+                    )
                          {
                              Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                             disableall();
                          }
+
                 }
 
 
@@ -107,27 +131,149 @@ public class Start_game extends AppCompatActivity implements View.OnClickListene
                break;
             case R.id.r12:
                 toggle(r12);
+
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r11str.equals(r12str) && r11str.equals(r13str))
+                                    ||(r12str.equals(r22str) && r12str.equals(r32str))
+
+                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
+
+
+
+
                 break;
             case R.id.r13:
                 toggle(r13);
+
+
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r11str.equals(r12str) && r11str.equals(r13str))
+                                    ||(r13str.equals(r23str) && r13str.equals(r33str))
+                                    || (r13str.equals(r22str) && r13str.equals(r31str))
+                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
                 break;
             case R.id.r21:
                 toggle(r21);
+
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r21str.equals(r22str) && r21str.equals(r23str))
+                                    ||(r11str.equals(r21str) && r11str.equals(r31str))
+
+                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
                 break;
             case R.id.r22:
                 toggle(r22);
                 break;
             case R.id.r23:
                 toggle(r23);
+
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r13str.equals(r23str) && r13str.equals(r33str))
+                                    ||(r21str.equals(r22str) && r21str.equals(r23str))
+                                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
                 break;
             case R.id.r31:
                 toggle(r31);
+
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r31str.equals(r32str) && r31str.equals(r33str))
+                                    ||(r11str.equals(r21str) && r11str.equals(r31str))
+                                    || (r31str.equals(r22str) && r31str.equals(r13str))
+                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
                 break;
             case R.id.r32:
                 toggle(r32);
+
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r31str.equals(r32str) && r31str.equals(r33str))
+
+                                    || (r12str.equals(r22str) && r12str.equals(r32str))
+                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
                 break;
             case R.id.r33:
                 toggle(r33);
+                if(count>4)
+                {
+                    //                  r11str=r11.getText().toString();
+                    updateText();
+                    if(
+                            (r31str.equals(r32str) && r31str.equals(r33str))
+                                    ||(r13str.equals(r23str) && r13str.equals(r33str))
+                                    || (r11str.equals(r22str) && r11str.equals(r33str))
+                    )
+                    {
+                        Toast.makeText(getApplicationContext(),"you won",Toast.LENGTH_SHORT).show();
+                        disableall();
+                    }
+
+                }
+
                 break;
 
         }
